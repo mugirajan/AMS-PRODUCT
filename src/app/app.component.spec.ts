dfd04f2aff1e460af80ba683774ae8e0459ad234
Kamal Raj <kamalrajganesan2000@gmail.com>
2024-06-12 22:50:19 +0530

    angular json edited and made standalone false

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'whatsappMessage' title`, () => {
+  it(`should have the 'dishoom' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('whatsappMessage');
+    expect(app.title).toEqual('dishoom');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, whatsappMessage');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, dishoom');
   });
 });
